3e9e24db7da7944995b7815b18f4fa9d580f8386
JenseyJim <jenseystevens15@gmail.com>
2024-08-29 19:43:31 -0400

    Ejercicio1-ResumenArreglos

---
+++ Ejercicio1-ResumenArreglos/Program.cs
@@ -0,0 +1,11 @@
+﻿//Ejemplo 1: Calcular la media de un conjunto de números
+
+int[] numbers = { 10, 20, 30, 40, 50 };
+int sum = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    sum += numbers[i];
+}
+double average = (double)sum / numbers.Length;
+Console.WriteLine($"La media es: {average}");
+
