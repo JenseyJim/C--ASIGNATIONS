044478231ae56cb97d7ffc520402a948da1d1c5c
JenseyJim <jenseystevens15@gmail.com>
2024-08-29 19:45:34 -0400

    Ejercicio2-ResumenArreglos

---
+++ Ejercicio2-ResumenArreglos/Program.cs
@@ -0,0 +1,10 @@
+﻿// Ejemplo 2: Invertir un arreglo
+
+int[] arr = { 1, 2, 3, 4, 5 };
+Array.Reverse(arr);
+Console.WriteLine("Arreglo invertido: ");
+foreach (int num in arr)
+{
+    Console.Write(num + " ");
+}
+
