5061b8f8421efd68b5e726c540b7417e183bfa2c
JenseyJim <jenseystevens15@gmail.com>
2024-08-29 20:11:45 -0400

    Ejercicio8-ResumenArreglos

--- Ejercicio8-ResumenArreglos/Program.cs
+++ Ejercicio8-ResumenArreglos/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Arreglos multidimensionales
+
+int[,] matriz = new int[2, 3] { { 1, 2, 3 }, { 4, 5, 6 } };
+for (int i = 0; i < matriz.GetLength(0); i++)
+{
+    for (int j = 0; j < matriz.GetLength(1); j++)
+    {
+        Console.Write(matriz[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.ReadKey();
