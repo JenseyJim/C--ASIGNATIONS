97a0ff765ebbe6945d43265770fc6fefca4471f2
JenseyJim <jenseystevens15@gmail.com>
2024-10-26 20:46:50 -0400

    C# Basico

---
+++ Basico/EX3-Alumnos/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Alumno al = new Alumno();
+        al.calcularPromedio([10, 20]);
+        al.mostrarInformacion("Stevens", "2023-1800", "Software");
+        Console.ReadKey();
+    }
+
